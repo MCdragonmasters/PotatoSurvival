25e941a9d1a9b2905a8711a34f3f0f298da66462
MCdragonmasters <natedragonmaster@gmail.com>
2024-12-10 15:45:48 -0500

    fr

--- src/main/java/com/mcdragonmasters/potatosurvival/PotatoSurvival.java
+++ src/main/java/com/mcdragonmasters/potatosurvival/PotatoSurvival.java
@@ -2,6 +2,7 @@ package com.mcdragonmasters.potatosurvival;
 
 import com.mcdragonmasters.potatosurvival.commands.RegisterCommands;
 import com.mcdragonmasters.potatosurvival.jsonDatabase.HomesManager;
+import com.mcdragonmasters.potatosurvival.jsonDatabase.WarpsManager;
 import com.mcdragonmasters.potatosurvival.listeners.RegisterListeners;
 import com.mcdragonmasters.potatosurvival.utils.Logger;
 import dev.jorel.commandapi.CommandAPI;
@@ -11,12 +12,11 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
 
-@SuppressWarnings({"unused", "deprecation", "CallToPrintStackTrace"})
+@SuppressWarnings({"unused", "deprecation"})
 public class PotatoSurvival extends JavaPlugin {
 
-    public static String nameSpace = "potatosurvival";
+    public final static String nameSpace = "potatosurvival";
     public static String prefix = ChatColor.GOLD + "Potato Survival" + ChatColor.GRAY + " >" + ChatColor.RESET;
     public static PotatoSurvival instance;
     public static FileConfiguration config;
@@ -34,17 +34,8 @@ public class PotatoSurvival extends JavaPlugin {
         instance = this;
         CommandAPI.onEnable();
 
-        File homesFile = new File(getDataFolder(), "homes.json");
-        if (!homesFile.exists()) {
-            try {
-                // Create the file if it does not exist
-                boolean newFile = homesFile.createNewFile();
-            } catch (Exception e) {
-                getLogger().warning("Could not create homes.json file!");
-                e.printStackTrace();
-            }
-        }
         HomesManager.loadHomes();
+        WarpsManager.loadWarps();
 
         //Register Event RegisterListeners
         RegisterListeners.register(getServer());
